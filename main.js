70a3720ea4db0c0b61785e87eb72dc0931ca7956
Leo Kosola <leokosola@gmail.com>
2020-10-21 18:50:46 +0300

    harmaat boksit done, aloitettu hahmottelu muihin osioihin

--- main.js
+++ main.js
@@ -1,48 +1,53 @@
-const app_bytes_raw = "GET/HTTP/1.1Host:tommitenkanen.netConnection:keep-aliveUpgrade-Insecure-Requests:1User-Agent:Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/86.0.4240.75Safari/537.36Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9Accept-Encoding:gzip,deflateAccept-Language:fi-FI,fi;q=0.9,en-US;q=0.8,en;q=0.7";
-const splitted_text = app_bytes_raw.split('');
-console.log(splitted_text);
-
 // APPLICATION LAYER
+
+const app_bytes_raw = "GET/HTTP/1.1Host:tommitenkanen.netConnection:keep-aliveUpgrade-Insecure-Requests:1User-Agent:Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/86.0.4240.75Safari/537.36Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9Accept-Encoding:gzip,deflateAccept-Language:fi-FI,fi;q=0.9,en-US;q=0.8,en;q=0.7";
+
+const app_bytes_array = app_bytes_raw.split('');
  
-const app_len = 456;
 const app_c = '#CCCCCC';
  
 const app_waffle = d3.select('.app_waffle');
-const app_numbers = d3.range(app_len);
-const letter = ['g', 'e', 't'];
  
 app_waffle
   .selectAll('.block')
-  .data(app_numbers)
+  .data(app_bytes_array)
   .enter()
   .append('div')
   .attr('class', 'block')
   .style('background-color', app_c)
-  .style('opacity', 0)
+  .style('opacity', 1)
+  // .style('opacity', 0)
   .style('outline', (d, i) => {
     if(i === 0 || i === 1 || i === 2) { 
 			return '1px solid black';
     }
   })
   .transition()
-  .delay((d, i) => { return i * 5; })
-  .style('opacity', 1);;
+  .delay((d, i) => { return i * 15; })
+  .text((d) => { return d; });
  
 // TRANSPORT LAYER
 
-const tra_len = 20;
-const tra_c = '#CCCCCC';
+const tra_byt_raw = 'ddfc0050469b7e115851524250180044d4bd0000';
+const tra_byt_array = [];
 
+for(let i = 0; i < tra_byt_raw.length - 1; i++) {
+  tra_byt_array.push(
+    tra_byt_raw[i] + tra_byt_raw[i + 1]);
+    i = i + 1;
+}
+
+const tra_c = '#CCCCCC';
 const tra_waffle = d3.select('.tra_waffle');
-const tra_numbers = d3.range(tra_len);
 
 tra_waffle
   .selectAll('.block')
-  .data(tra_numbers)
+  .data(tra_byt_array)
   .enter()
   .append('div')
   .attr('class', 'block')
   .style('background-color', tra_c)
+  .text((d => { return d; }))
   .style('opacity', 0)
   .transition()
   .delay((d, i) => { return i * 20; })
@@ -50,19 +55,26 @@ tra_waffle
  
 // NETWORK LAYER
  
-const net_len = 20;
-const net_c = '#CCCCCC';
+const net_byt_raw = '450001f071ee40008006d6cf82e95488bc751c63';
+const net_byt_array = [];
 
+for(let i = 0; i < net_byt_raw.length - 1; i++) {
+  net_byt_array.push(
+    net_byt_raw[i] + net_byt_raw[i + 1]);
+    i = i + 1;
+}
+
+const net_c = '#CCCCCC';
 const net_waffle = d3.select('.net_waffle');
-const net_numbers = d3.range(net_len);
 
 net_waffle
   .selectAll('.block')
-  .data(net_numbers)
+  .data(net_byt_array)
   .enter()
   .append('div')
   .attr('class', 'block')
   .style('background-color', net_c)
+  .text((d) => { return d; })
   .style('opacity', 0)
   .transition()
   .delay((d, i) => { return i * 20; })
@@ -70,19 +82,26 @@ net_waffle
 
 // LINK LAYER
  
-const lin_len = 14;
-const lin_c = '#CCCCCC';
+const lin_byt_raw = '00005e000101183da2f31c2c0800';
+const lin_byt_array = [];
 
+for(let i = 0; i < lin_byt_raw.length - 1; i++) {
+  lin_byt_array.push(
+    lin_byt_raw[i] + lin_byt_raw[i + 1]);
+    i = i + 1;
+}
+
+const lin_c = '#CCCCCC';
 const lin_waffle = d3.select('.lin_waffle');
-const lin_numbers = d3.range(lin_len);
 
 lin_waffle
   .selectAll('.block')
-  .data(lin_numbers)
+  .data(lin_byt_array)
   .enter()
   .append('div')
   .attr('class', 'block')
   .style('background-color', lin_c)
+  .text((d) => { return d; })
   .style('opacity', 0)
   .transition()
   .delay((d, i) => { return i * 20; })
@@ -91,23 +110,14 @@ lin_waffle
 
 // TESTI
 
-const letArray = ['A', 'B', 'C', 'D', 'E'];
-const vohveli = d3.select('.vohveli');
+// const letArray = ['A', 'B', 'C', 'D', 'E'];
+// const vohveli = d3.select('.vohveli');
 
-vohveli
-	.selectAll('.block')
-	.data(letArray)
-	.enter()
-	.append('div')
-	.attr('class', 'block')
-	.style('background-color', '#CCCCCC')
-	.text((d) => { return d; });
-
-const tavut = vohveli.selectAll('div');
-
-tavut
-	.data(letArray)
-	.enter()
-	.text((d) => { 
-		console.log(d);
-		return d; });
+// vohveli
+// 	.selectAll('.block')
+// 	.data(splitted_text)
+// 	.enter()
+// 	.append('div')
+// 	.attr('class', 'block')
+// 	.style('background-color', '#CCCCCC')
+// 	.text((d) => { return d; });
